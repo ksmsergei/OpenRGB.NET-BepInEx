e6d0869905995a550bfc5222ad8523cce2bf2777
ksmsergei <ksmsergei29@gmail.com>
2026-02-16 21:06:52 +0500

    chore: overhaul code structure

---
+++ OpenRGB_BepInEx.cs
@@ -0,0 +1,19 @@
+using BepInEx;
+using BepInEx.Logging;
+
+namespace OpenRGB_BepInEx;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class OpenRGBBepInEx : BaseUnityPlugin
+{
+    public static OpenRGBBepInEx Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Instance = this;
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+}
